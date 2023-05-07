1603d61e740af6005654be583b2be37fbe8f9286
Jorge Cano <coronero@gmail.com>
2023-05-07 11:52:38 -0700

    110 #1

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 import json
-from config import db #imports db variable from config.py
+from config import db  # imports db variable from config.py
 
 app = Flask(__name__)
+
 
 @app.get("/")
 def home():
@@ -18,7 +19,7 @@ def test():
 ##### JSON #####
 
 def fix_id(obj):
-    #fix the object to be json parsable
+    # fix the object to be json parsable
     obj["_id"] = str(obj["_id"])
     return obj
 
@@ -27,6 +28,7 @@ def fix_id(obj):
 def about():
     me = {"name": "Jorge Cano"}
     return json.dumps(me)
+
 
 @app.get("/api/catalog")
 def get_catalog():
@@ -37,6 +39,7 @@ def get_catalog():
 
     return json.dumps(products)
 
+
 @app.post("/api/catalog")
 def save_product():
     data = request.get_json()
@@ -45,8 +48,8 @@ def save_product():
     return json.dumps(fix_id(data))
 
 
-#get request on /api/total
-#return the total value of your catalog (sum of all prices)
+# get request on /api/total
+# return the total value of your catalog (sum of all prices)
 @app.get("/api/total")
 def get_total_price():
     total = 0
@@ -56,8 +59,10 @@ def get_total_price():
 
     return json.dumps(total)
 
-#get /api/products
-#return the number of products in the catalog
+# get /api/products
+# return the number of products in the catalog
+
+
 @app.get("/api/products")
 def num_products():
     ctr = 0
@@ -67,4 +72,97 @@ def num_products():
 
     return json.dumps(ctr)
 
-app.run(debug=True)
+
+# get /api/categories
+# should return the list of categories
+# retrieve all products, get the category and put it on a list
+# then return the list as json
+@app.get("/api/categories")
+def get_category():
+    categories = []
+    cursor = db.products.find({})
+    for prod in cursor:
+        cat = prod['category']
+        if cat not in categories:
+            categories.append(cat)
+
+    return json.dumps(categories)
+
+
+# declaring a variable in an endpoint
+@app.get("/api/products/categories/<name>")
+def get_by_category(name):
+    results = []
+    cursor = db.products.find({})
+    for prod in cursor:
+        if prod["category"] == name:
+            results.append(fix_id(prod))
+    return json.dumps(results)
+
+# get /api/products/search/test
+
+
+@app.get("/api/products/search/<term>")
+def search_products(term):
+    results = []
+    cursor = db.products.find({"title": {"$regex": term, "$options": "i"}})
+    for prod in cursor:
+        results.append(fix_id(prod))
+    return json.dumps(results)
+
+
+# create a get end point on /api/products/lower/value
+# to retrieve all products whose price is lower than given value
+@app.get("/api/products/lower/<value>")
+def price_lower(value):
+    results = []
+    value = float(value)
+    cursor = db.products.find({"price": {"$lt": value}})
+    for prod in cursor:
+        results.append(fix_id(prod))
+    return json.dumps(results)
+
+
+# to retrieve all products whose price is greater than or equal to given value
+@app.get("/api/products/greater/<value>")
+def price_greater(value):
+    results = []
+    value = float(value)
+    cursor = db.products.find({"price": {"$gte": value}})
+    for prod in cursor:
+        results.append(fix_id(prod))
+    return json.dumps(results)
+
+
+#################################
+######### COUPON CODE###########
+
+# GET /api/coupons - retrieve all
+# POST /api/coupon - save new
+# GET /api/coupon/<code> - search 1 by code
+@app.get("/api/coupons")
+def get_coupon():
+    coupons = []
+    cursor = db.coupons.find({})
+    for coup in cursor:
+        coupons.append(fix_id(coup))
+
+    return json.dumps(coupons)
+
+
+@app.post("/api/coupons")
+def save_coupons():
+    data = request.get_json()
+    db.coupons.insert_one(data)
+    return json.dumps(fix_id(data))
+
+
+@app.get("/api/coupons/<code>")
+def get_by_code(code):
+    coupon = db.coupons.find_one({"code": code})
+    if not coupon:
+        return abort(404, "Invalid Code")
+
+    return json.dumps(fix_id(coupon))
+
+# app.run(debug=True)
